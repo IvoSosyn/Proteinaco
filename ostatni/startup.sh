c80e1492fd68b069f32767d9662e9a317f0ccc1e
sosyn <sosyn@sosyn-w10.mukrnov.cz>
2020-06-10 07:12:54 +0200

    8. Stažené soubory z Debianu do adresáře "ostatní" 7. Doplněno logování podle tříd - rozděláno logování podle konfiguračního souboru 6. I. Finální verze - nutno dodělat lo4j do souboru 5. Testovací verze 4. Naprogramovat vlastní načítání ceníkových (skladových) položek 3. Normalizovat ceníkové položky a položky objednávek, nejlépe nějakým ukončovacím znakem "#" 2. Vyřešen zápis 1.Založení aplikace - po processXML - <ITEM> výměnu

--- ostatni/startup.sh
+++ ostatni/startup.sh
@@ -3,18 +3,17 @@
 # Start Script for java proteinaco.jar
 # -----------------------------------------------------------------------------
 
-	#echo CALASPAH=$CALSSPATH
+	#echo CLASSPATH=$CLASSPATH
 	#echo
 
 PRG=/usr/lib/jvm/java-1.11.0-openjdk-i386/bin/java
 PRGDIR=/home/sosyn
 EXECUTABLE=/home/sosyn/Proteinaco.jar
 CONFIG_FILE=/home/sosyn/proteinaco.config
-CP="/home/sosyn/log4j/log4j-core-2.13.3.jar;/home/sosyn/log4j/log4j-api-2.13.3.jar"
-
-echo =Start=============== `date "+%F %H:%M:%S"` >> proteinaco.log
+CLASSPATH=$CLASSPATH:/home/sosyn/log4j/log4j-core-2.13.3.jar:/home/sosyn/log4j/log4j-api-2.13.3.jar
 
 cd $PRGDIR
-$PRG -classpath $CP -jar $EXECUTABLE $CONFIG_FILE >> proteinaco.log
+echo =Start=============== `date "+%F %H:%M:%S"` >> proteinaco.log
+$PRG -classpath $CLASSPATH -jar $EXECUTABLE $CONFIG_FILE >> proteinaco.log
 	#$PRG $CP -jar $EXECUTABLE $CONFIG_FILE 
 echo =Stop=============== `date "+%F %H:%M:%S"` >> proteinaco.log
